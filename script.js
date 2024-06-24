7ffd14a23f515359c3a8add57d7aa67e0a898cc2
Nelly Njeri <nellycyberpro@gmail.com>
2024-06-24 23:37:14 +0300

    Initial commit amidst rejecting financebill2024

---
+++ script.js
@@ -0,0 +1,97 @@
+// script.js
+
+const quizData = [
+    {
+        question: "What is the capital of France?",
+        a: "Berlin",
+        b: "Madrid",
+        c: "Paris",
+        d: "Lisbon",
+        correct: "c",
+    },
+    {
+        question: "Who is the CEO of Tesla?",
+        a: "Jeff Bezos",
+        b: "Elon Musk",
+        c: "Bill Gates",
+        d: "Tony Stark",
+        correct: "b",
+    },
+    {
+        question: "The iPhone was created by which company?",
+        a: "Apple",
+        b: "Intel",
+        c: "Amazon",
+        d: "Microsoft",
+        correct: "a",
+    },
+    {
+        question: "How many Harry Potter books are there?",
+        a: "6",
+        b: "7",
+        c: "8",
+        d: "9",
+        correct: "b",
+    }
+];
+
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false);
+}
+
+function getSelected() {
+    let answer;
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+    return answer;
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++;
+       }
+
+       currentQuiz++;
+
+       if(currentQuiz < quizData.length) {
+           loadQuiz();
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+           <button onclick="location.reload()">Reload</button>
+           `;
+       }
+    }
+});
